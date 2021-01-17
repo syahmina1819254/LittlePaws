528b90a67df71a371f27eff116fbf4055a29e637
syahmina1819254 <55779851+syahmina1819254@users.noreply.github.com>
2021-01-17 15:37:22 +0800

    Update pet.php

--- Code Syahmina/Model/pet.php
+++ Code Syahmina/Model/pet.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class pet extends Model
 {
     protected $table='pet_adoptions';
+    protected $fillable=['petID','Age','petName','petType','pet_image'];
+    protected $primaryKey = 'petID';
     
 
     public function adoptionReq(){
